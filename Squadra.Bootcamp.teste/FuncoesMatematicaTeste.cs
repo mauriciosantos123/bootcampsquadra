a4b926026c4523c532b51c95ec2cb19638070f77
Mauricio Vieira <mauriciusantos123@hotmail.com>
2021-09-05 13:23:12 -0300

    frist

--- Squadra.Bootcamp.teste/FuncoesMatematicaTeste.cs
+++ Squadra.Bootcamp.teste/FuncoesMatematicaTeste.cs
@@ -14,8 +14,7 @@ namespace Squadra.Bootcamp.teste
         public void Soma_DeveRetornarOValorCorreto()
         {
             FuncaoMatematica c = new();
-            var resultado = c.Somar(10, 20);
-            //Verifica se o resultado é igual a 30        
+            var resultado = c.Somar(10, 20);       
             Assert.Equal(30, resultado);
         }
 
@@ -23,12 +22,33 @@ namespace Squadra.Bootcamp.teste
         [InlineData(1 , 2, 0.5)]
         [InlineData(2, 2, 1)]
         [InlineData(10 ,5, 2)]
-        public void RestoDivisao_DeveRetornarZero(int valor_1 ,int valor_2,int total)
+        public void Divisao_DeveRetornarValorCorreto(int valor_1 ,int valor_2,int total)
         {
             FuncaoMatematica c = new ();
             var resultado = c.Dividir(valor_1, valor_2);
-            //Verifica se o resto da divisão é 0
             Assert.Equal(total, resultado) ;
+        }
+
+        [Theory]
+        [InlineData(1, 3, 1)]
+        [InlineData(2, 2, 0)]
+        [InlineData(10, 5, 5)]
+        public void Subitracao_TrazerValorCorreto(int valor_1, int valor_2, int total)
+        {
+            FuncaoMatematica c = new();
+            var resultado = c.Subitrair(valor_1, valor_2);
+            Assert.Equal(total, resultado);
+        }
+
+        [Theory]
+        [InlineData(2, 2, 4)]
+        [InlineData(2, 2, 0)]
+        [InlineData(10, 5, 50)]
+        public void Multiplicacao_trazerValorCorreto(int valor_1, int valor_2, int total)
+        {
+            FuncaoMatematica c = new();
+            var resultado = c.Multiplicar(valor_1, valor_2);
+            Assert.Equal(total, resultado);
         }
     }
 }
